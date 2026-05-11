982f674dce66cfbf3fdafce7f1d90381e1be8ac7
blaisedavis21 <blaisedavis207@gmail.com>
2026-05-11 22:36:45 +0300

    Implemented Collatz algorithm to generate sequence of intermediate values

--- exercises/basics.py
+++ exercises/basics.py
@@ -1,18 +1,15 @@
 from typing import List
 
 
-def collatz(n: int) -> List[int]:
-    """
-    You're given a positive integer n. Write an algorithm that does the following:
-        - If n is even, the algorithm divides n by 2. This is the new value of n
-        - If n is odd, the algorithm multiplies it by 3 and adds 1. This is the new value of n.
-        - The algorithm repeats this until n == 1.
-
-    Implement this algorithm in this function and return a list of all the intermediate values of n.
-    For example, if n = 3, the sequence of values is: 3 -> 10 -> 5 -> 16 -> 8 -> 4 -> 2 -> 1
-    So, your function would return: [3, 10, 5, 16, 8, 4, 2, 1]
-    """
-    pass
+def collatz(n):
+    sequence = [n]
+    while n != 1:
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = 3 * n + 1
+        sequence.append(n)
+    return sequence
 
 
 def distinct_numbers(numbers: List[int]) -> int:
